9074c092714ba3883f540498cede4ccb4a9b0d37
abdul jarkoni <abduljarkoni.aj@gmail.com>
2021-02-20 13:47:38 +0700

    belajar css display

---
+++ main.js
@@ -0,0 +1,8 @@
+//mengatur css display
+const cssDisplay = (event) => {
+  //tangkap select value nya
+  let selecValue = document.getElementById("css_display").value;
+
+  //tampilkan di log
+  console.log(selecValue);
+};
